13bee850e3d04b39ccbe925d40717b0a589266db
Nicolas Mariangel <nicolas.mariangel@usach.cl>
2017-08-22 21:15:05 -0400

    Fixed labels in figures, de-normalized fft when inversing, setting correct indexes to fftfreq when setting to 0

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-
+import numpy as np
 from numpy import sin, linspace, pi
 from pylab import plot, show, title, xlabel, ylabel, subplot
-from scipy import ifft, fft, arange
-from scipy.io.wavfile import read
+from scipy import *
+from scipy.io.wavfile import read, write
 
 
 
@@ -13,44 +13,48 @@ def plotSpectrum(y,Fs):
  	n = len(y) # length of the signal
  	k = arange(n)
  	T = n/Fs
- 	frq = k/T # two sides frequency range
- 	frq = frq[range(round(n/2))] # one side frequency range
+ 	frq = np.fft.fftfreq(n, 1/Fs)
 
  	Y = fft(y)/n # fft computing and normalization
- 	Y = Y[range(round(n/2))]
+ 	#Y = Y[range(round(n/2))]
 	 
  	#plot(frq,abs(Y),'r') # plotting the spectrum
  	#xlabel('Freq (Hz)')
  	#ylabel('|Y(freq)|')
-
- 	maxFreqI = 0;
- 	maxFreq = 0;
+ 	
+ 	maxFreqIl = 0
+ 	maxFreqIr = 0
+ 	maxFreq = 0
  	for i in range(len(Y)):
- 		if Y[i] > maxFreq:
- 			maxFreq = Y[i]
- 			maxFreqI = i
+ 		if int(Y[i]) > maxFreq:
+ 			maxFreq = int(Y[i])
+ 			maxFreqIl = i
 
- 	n = len(Y)
- 	left = round(maxFreqI - maxFreqI*0.15)
- 	if(left < 0):
- 		left = 0
- 	right = round(maxFreqI + maxFreqI*0.15)
- 	if(right >= n):
- 		right = n-1
+ 	for i in range(len(Y)):
+ 		if int(Y[i]) == maxFreq and i != maxFreqIl:
+ 			maxFreqIr = i
+ 			break
+
+ 	left = maxFreqIl + 1
+ 	right = maxFreqIr - 1
 
  	shortSpectrum = Y
- 	for i in range(n):
- 		if(i < left or i > right):
+ 	for i in range(int(n/2)):
+ 		if i > left:
  			shortSpectrum[i] = 0
- 	print (maxFreq, n, left, right, len(shortSpectrum))
+
+ 	for i in range(int(n/2)+1, n):
+ 		if i < right:
+ 			shortSpectrum[i] = 0
 
  	plot(frq, abs(shortSpectrum), 'r')
- 	xlabel('Freq (Hz)')
- 	ylabel('|Y(freq)|')
+ 	xlabel('Frecuencia (Hz)')
+ 	ylabel('Amplitud [dB]')
 
  	return shortSpectrum
 
-freq, info = read("lab1-1.wav")
+filename = "lab1-1"
+freq, info = read(filename + ".wav")
 
 Ts =len(info)/freq; # sampling interval
 t = linspace(0,Ts,len(info)) # time vector
@@ -60,13 +64,17 @@ t = linspace(0,Ts,len(info)) # time vector
 
 subplot(3,1,1)
 plot(t,info)
-xlabel('Time')
-ylabel('Amplitude')
+xlabel('Tiempo [s]')
+ylabel('Amplitud')
 subplot(3,1,2)
 shortSpectrum = plotSpectrum(info,freq)
 subplot(3,1,3)
-t2 = linspace(0, Ts, len(shortSpectrum))
-plot(t2,ifft(shortSpectrum))
-xlabel('Time')
-ylabel('Amplitude')
+Z = ifft(shortSpectrum)*len(info) #desnornalizar
+#print(shortSpectrum)
+#print(Z)
+plot(t, Z, 'g')
+xlabel('Tiempo [s]')
+ylabel('Amplitud')
 show()
+
+write(filename + "-inverse.wav", freq, Z.astype(info.dtype))
